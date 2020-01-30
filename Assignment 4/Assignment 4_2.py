a943e794a330b12c5e2f69a7869f9b2ee71ff678
Dave Braunschweig <dbraunsc@harpercollege.edu>
2020-01-30 08:58:19 -0600

    Assignment 4 code formatting

--- Assignment 4/Assignment 4_2.py
+++ Assignment 4/Assignment 4_2.py
@@ -3,21 +3,19 @@
 
 print("Please Enter Distance in Miles : ")
 mile = float(input())
+
 yards = mile * 1760
 feet = mile * 5280
 inch = mile * 63360
+
 centimeter = mile * 160934
 meter = centimeter / 100
 kilometer = meter / 1000
 
 print("Your distance in yards:" + str(yards) + " Yds")
-
 print("Your distance in feets:" + str(feet) + " Fts")
-
 print("Your distance in inches:" + str(inch) + " Inches")
 
 print("Your distance in Kilometer:" + str(kilometer) + " Kms")
-
 print("Your distance in meter:" + str(meter) + " Meters")
-
 print("Your distance in centimeter:" + str(centimeter) + " Cms")
