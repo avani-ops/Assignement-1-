e3eb4f24bd6cb02e51a4d4a4af41ed013f0ef6a4
avani-ops <59943694+avani-ops@users.noreply.github.com>
2020-02-13 01:57:52 -0600

    Add files via upload

---
+++ Assignment 5/Assignment 5_2.py
@@ -0,0 +1,48 @@
+def displayDays(days):
+    print(" Your age in days : " + str(days) + " days")
+
+def displayHours(hours):
+    print(" Your age in hours : " + str(hours) + " Hours")
+
+def displayMonths(months):
+    print(" Your age in months : " + str(months) + " Months")
+
+def displaySeconds(seconds):
+    print(" Your age in seconds : " + str(seconds) + " Secs")
+
+def getAge():
+    print("Please enter you age in years : ")
+    age = float(input())
+    
+    return age
+
+def getDays(age):
+    days = age * 365
+    
+    return days
+
+def getHours(age):
+    hours = age * 8760
+    
+    return hours
+
+def getMonths(age):
+    months = age * 12
+    
+    return months
+
+def getSeconds(age):
+    seconds = age * 31556952
+    
+    return seconds
+
+# Main
+age = getAge()
+days = getDays(age)
+months = getMonths(age)
+hours = getHours(age)
+seconds = getSeconds(age)
+displayMonths(months)
+displayDays(days)
+displayHours(hours)
+displaySeconds(seconds)
