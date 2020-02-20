338d10d310897d1045cbbe4c0f204676d0ec4597
avani-ops <59943694+avani-ops@users.noreply.github.com>
2020-02-20 02:48:32 -0600

    Update Activity 4.py

--- Assignment 6/Activity 4.py
+++ Assignment 6/Activity 4.py
@@ -2,46 +2,43 @@
 # count the area  of the room using function and display using the function
 
 
-def getLength():
+def get_length():
     print("Please enter the length of the room(in feets)")
     length = float(input())
     return length
 
 
-def getWidth():
+def get_width():
     print("Please enter the Width of the room(in feets)")
     width = float(input())
     return width
 
 
-def getArea(length, width):
+def get_area(length, width):
     area = length * width
     return area
 
 
-def displayArea(area):
+def display_area(area):
     print(f"The area of room is {area :.2f} square feets")
-    
 
-def getYard(area):
+
+def get_yard(area):
     feet = area // 9
     yard = area - (feet * 9)
-    displayYard(feet, yard)
+    display_yard(feet, yard)
 
 
-def displayYard(feet, yard):
+def display_yard(feet, yard):
     print(f"The area of room is {feet: .0f} square feets and {yard: .2f} square yards")
 
 
 def main():
-    Length = getLength()
-    Width = getWidth()
-
-    Area = getArea(Length, Width)
-
-    displayArea(Area)
-
-    getYard(Area)
+    length = get_length()
+    width = get_width()
+    area = get_area(length, width)
+    display_area(area)
+    get_yard(area)
 
 # main function body
 main()
