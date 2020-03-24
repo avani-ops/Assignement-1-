699cb9c801544395a4752f5b51430dfa9bb7547b
avani-ops <59943694+avani-ops@users.noreply.github.com>
2020-03-24 17:58:08 -0500

    Update Activity 2.py

--- Assignment 11/Activity 2.py
+++ Assignment 11/Activity 2.py
@@ -1,25 +1,32 @@
+# This program will take static array from user and
+# display in descending order
+
+
 def getMarks():
     print("Please enter score:")
-    marks = int(input())    
+    marks = int(input())
     return marks
+
 
 def getSubject():
     print("Please Enter how many score you want to enter ?")
     subject = int(input())
     return subject
 
+
 def getSort(Marks, score):
     x = 1
-    while x < score :
+    while x < score:
         y = x + 1
         while y <= score:
             if(Marks[x] > Marks[y]):
                 temp = Marks[x]
-                Marks[x] =  Marks[y]
+                Marks[x] = Marks[y]
                 Marks[y] = temp
             y = y + 1
         x = x + 1
     return Marks
+
 
 def getOutPut(marks, subject):
     print("Grade in highest to minimum range")
@@ -28,7 +35,7 @@ def getOutPut(marks, subject):
         print(str(marks[flag]))
         flag = flag - 1
 
-    
+
 def main():
     subject = getSubject()
     flag = 0
