fd91e46e8ed5c40c945872b33496708bb8ef26f6
avani-ops <59943694+avani-ops@users.noreply.github.com>
2020-03-26 01:28:59 -0500

    Update Activity 1.py

--- Assignment 11/Activity 1.py
+++ Assignment 11/Activity 1.py
@@ -1,55 +1,61 @@
-def getAverage(marks, subject):
+# This program takes grades from user store in array and
+# dislay highest , lowest and average grade.
+
+
+def get_average(marks, subject):
     total = 0
-    for flag in range(0, subject - 1 + 1, 1):
+    for flag in range(0, subject, 1):
         total = total + marks[flag]
     average = float(total) / subject
-    
     return average
 
-def getMarks():
+
+def get_marks():
     print("Please enter Grades :")
     marks = int(input())
-    
     return marks
 
-def getMax(marks, subject):
+
+def get_max(marks, subject):
     max = 0
-    for flag in range(0, subject - 1 + 1, 1):
+    for flag in range(0, subject, 1):
         if max < marks[flag]:
             max = marks[flag]
-    
     return max
 
-def getMin(marks, subject):
+
+def get_min(marks, subject):
     min = marks[1]
-    for flag in range(0, subject - 1 + 1, 1):
+    for flag in range(0, subject, 1):
         if min > marks[flag]:
             min = marks[flag]
-    
     return min
 
-def getOutPut(max, min, average):
+
+def get_outPut(max, min, average):
     print("Maximum Grade : " + str(max))
     print("Minimum Grade : " + str(min))
     print("Average Grade : " + str(average))
 
-def getSubject():
+
+def get_subject():
     print("Please Enter how many subjects you want to enter ?")
     subject = int(input())
-    
     return subject
 
-# Main
-# This program takes grades from user store in array and islay highest , lowest and average grade.
-subject = getSubject()
-marks = [0] * (subject)
 
-flag = 0
-while flag < subject:
-    marks[flag] = getMarks()
-    print(marks[flag])
-    flag = flag + 1
-max = getMax(marks, subject)
-min = getMin(marks, subject)
-average = getAverage(marks, subject)
-getOutPut(max, min, average)
+def main():
+    subject = get_subject()
+    marks = [0] * (subject)
+
+    flag = 0
+    while flag < subject:
+        marks[flag] = get_marks()
+        flag = flag + 1
+    max = get_max(marks, subject)
+    min = get_min(marks, subject)
+    average = get_average(marks, subject)
+    get_outPut(max, min, average)
+
+
+main()
