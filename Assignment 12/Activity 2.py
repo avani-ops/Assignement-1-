41cb41d50abea933e2c04322a9284a263def336e
avani-ops <59943694+avani-ops@users.noreply.github.com>
2020-04-01 00:41:59 -0500

    Update Activity 2.py

--- Assignment 12/Activity 2.py
+++ Assignment 12/Activity 2.py
@@ -1,43 +1,50 @@
-# This program will take dynamic array from user and
-# display in descending order
+# This program will ask user to guess one number
+# between 1 - 100 and using guuessing mid point
+# find the correct number and display the number 
+# of guesses
 
 
-def get_marks():
-    print("Please enter score:")
-    marks = int(input())
-    return marks
+def get_answer(max_no, min_no, mid_no):
+    guess = []
+    while True:
+        choice = get_choice(mid_no)
+        if choice == 1:
+            guess = get_guess(guess, mid_no)
+            min_no = mid_no
+            mid_no = int((min_no + max_no)/2)
+        elif choice == 2:
+            guess = get_guess(guess, mid_no)
+            max_no = mid_no
+            mid_no = int((min_no + max_no)/2)
+        elif choice == 3:
+            guess = get_guess(guess, mid_no)
+            break
+    return guess
 
 
-def get_subject():
-    print("Please Enter how many score you want to enter ?")
-    subject = int(input())
-    return subject
+def get_guess(guess, new_guess):
+    guess.append(new_guess)
+    return guess
 
 
-def get_sort(marks, score):
-    marks.sort()
-    marks.reverse()
-    return marks
-
-
-def get_output(marks, subject):
-    print("Grade in highest to minimum range")
-    for i in range(0, subject, 1):
-        print(marks[i])
+def get_choice(mid_no):
+    print(" Enter '1' if your number is greater than :" + str(mid_no))
+    print(" Enter '2' if your number is less than: " + str(mid_no))
+    print(" Enter '3' if you number is equal to: " + str(mid_no))
+    choice = int(input())
+    return choice
 
 
 def main():
-    flag = 0
-    marks = []
-    while True:
-        score = get_marks()
-        if score <= 0:
-            break
-    marks.append(score)
-    flag = flag + 1
-
-    marks = get_sort(marks, flag)
-    get_output(marks, flag)
+    guess = []
+    print("Please select one number between 1 - 100")
+    min_no = 0
+    max_no = 100
+    mid_no = 50
+    guess = get_answer(max_no, min_no, mid_no)
+    print("Total Guess made : " + str(len(guess)))
+    print("Total guesses are mentioned below ")
+    print(guess)
 
 
 main()
