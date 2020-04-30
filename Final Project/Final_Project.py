751f7dc91ef0e20f061aafcc669b59b806add24a
avani-ops <59943694+avani-ops@users.noreply.github.com>
2020-04-30 05:46:07 -0500

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -12,9 +12,52 @@ catalog_data = []
 for line in cd_data.split("\n"):
     catalog_data.append(line)
 
-print(catalog_data)
+title_data = []
+artist_data= []
+company_data = []
+country_data = []
+price_data = []
+year_data = []
 
 for i in range(0,len(catalog_data)-1,1):
     check = catalog_data[i].find("TITLE")
     if check >= 0:
+        title_data.append(catalog_data[i])
         print(catalog_data[i])
+        
+for i in range(0,len(catalog_data)-1,1):
+    check = catalog_data[i].find("ARTIST")
+    if check >= 0:
+        artist_data.append(catalog_data[i])
+        print(catalog_data[i])
+
+for i in range(0,len(catalog_data)-1,1):
+    check = catalog_data[i].find("COMPANY")
+    if check >= 0:
+        company_data.append(catalog_data[i])
+        print(catalog_data[i])
+
+
+for i in range(0,len(catalog_data)-1,1):
+    check = catalog_data[i].find("COUNTRY")
+    if check >= 0:
+        country_data.append(catalog_data[i])
+        print(catalog_data[i])
+
+
+for i in range(0,len(catalog_data)-1,1):
+    check = catalog_data[i].find("PRICE")
+    if check >= 0:
+        price_data.append(catalog_data[i])
+        print(catalog_data[i])
+
+for i in range(0,len(catalog_data)-1,1):
+    check = catalog_data[i].find("YEAR")
+    if check >= 0:
+        year_data.append(catalog_data[i])
+        print(catalog_data[i])
+        
+#for j in range(0, len(title_data)-1 , 1):
+    
+
+
