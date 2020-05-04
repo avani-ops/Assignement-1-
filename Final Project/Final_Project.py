29649d3b00af27a8ef05eb802fef6f68ef757471
avani-ops <59943694+avani-ops@users.noreply.github.com>
2020-05-04 18:18:24 -0500

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -12,17 +12,36 @@ def fetch_info():
         exit(1)
     return main_data
 
+def get_title_array(record):
+    get_record = record.split("\n")
+    data = ""
+    for i in range(0,len(get_record),1):
+        if(get_record[i].find("TITLE") > 0):
+            end  = get_record[i].find("</TITLE>")
+            data = get_record[i][:end]
+    return data
+        
+
+
+def get_data_array(main_data):
+    title_data = []
+    for i in range(1,len(main_data)-1,1):
+        title_data.append(get_title_array(main_data[i]))
+    
+    print(title_data)
+
 
 def main():
     main_data = fetch_info()
     print(main_data)
-    
-    
-    
+
+
+
 main()
 
 
-catalog_data = []
+
+'''catalog_data = []
 for line in cd_data.split("\n"):
     catalog_data.append(line)
 
@@ -71,4 +90,6 @@ for i in range(0,len(catalog_data)-1,1):
         year_data.append(catalog_data[i])
         print(catalog_data[i])
         
+#for j in range(0, len(title_data)-1 , 1):
+    '''
 
