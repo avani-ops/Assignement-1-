af1dc4f62a1768b50d4ab9adc2b647d7e9a69ba7
avani-ops <59943694+avani-ops@users.noreply.github.com>
2020-05-05 04:01:05 -0500

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -94,6 +94,10 @@ def get_price_array(record):
             start = get_record[i].find(">") + 1
             end  = get_record[i].find("</PRICE>")
             data = get_record[i][start:end]
+            try:
+                data =  float(data)
+            except :
+                data = 0.00
     return data
         
 
@@ -123,9 +127,23 @@ def get_data_year(main_data):
         year_data.append(get_year_array(main_data[i]))
     return year_data
 
+
 def print_output(title_data,artist_data,country_data,company_data,price_data,year_data):
     for i in range (0, len(title_data),1):
         print(f"{title_data[i]} - {artist_data[i]} - {country_data[i]} - {company_data[i]} - $ {price_data[i]} - {year_data[i]}")
+
+
+def get_data_average(price):
+    total = 0
+    for i in range(0,len(price),1):
+        total = total + price[i]
+    
+    average = total / len(price)
+    return average
+
+def print_output_average(record,average):
+    print(f" Total Records Available in Catalog : {record}")
+    print(f" Average price of items in Catalog  : {average :.2f}")
 
 def main():
     main_data = fetch_info()
@@ -136,60 +154,8 @@ def main():
     price_data = get_data_price(main_data)
     year_data = get_data_year(main_data)
     print_output(title_data,artist_data,country_data,company_data,price_data,year_data)
+    average_data = get_data_average(price_data)
+    print_output_average(len(title_data),average_data)
 
 main()
-
-
-
-'''catalog_data = []
-for line in cd_data.split("\n"):
-    catalog_data.append(line)
-
-title_data = []
-artist_data= []
-company_data = []
-country_data = []
-price_data = []
-year_data = []
-
-for i in range(0,len(catalog_data)-1,1):
-    check = catalog_data[i].find("TITLE")
-    if check >= 0:
-        title_data.append(catalog_data[i])
-        print(catalog_data[i])
-        
-for i in range(0,len(catalog_data)-1,1):
-    check = catalog_data[i].find("ARTIST")
-    if check >= 0:
-        artist_data.append(catalog_data[i])
-        print(catalog_data[i])
-
-for i in range(0,len(catalog_data)-1,1):
-    check = catalog_data[i].find("COMPANY")
-    if check >= 0:
-        company_data.append(catalog_data[i])
-        print(catalog_data[i])
-
-
-for i in range(0,len(catalog_data)-1,1):
-    check = catalog_data[i].find("COUNTRY")
-    if check >= 0:
-        country_data.append(catalog_data[i])
-        print(catalog_data[i])
-
-
-for i in range(0,len(catalog_data)-1,1):
-    check = catalog_data[i].find("PRICE")
-    if check >= 0:
-        price_data.append(catalog_data[i])
-        print(catalog_data[i])
-
-for i in range(0,len(catalog_data)-1,1):
-    check = catalog_data[i].find("YEAR")
-    if check >= 0:
-        year_data.append(catalog_data[i])
-        print(catalog_data[i])
-        
-#for j in range(0, len(title_data)-1 , 1):
-    '''
 
