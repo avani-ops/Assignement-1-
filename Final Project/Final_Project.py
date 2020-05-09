34af877f19a8b31f3808190b0d9df2d45d04fcb3
Dave Braunschweig <dave@chicagocoding.com>
2020-05-09 09:44:37 -0500

    Final project corrections

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -41,13 +41,45 @@ def check_price(price):
         try:
             price[i] = float(price[i])
         except:
+            print(f"Invalid price data: price[{i}] = {price[i]}")
             price[i] = 0.00
     return price
 
 
 def print_output(title_data, artist_data, country_data, company_data, price_data, year_data):
-    display_range = min(len(title_data),len(artist_data),len(country_data),len(company_data),len(price_data),len(year_data))
-    for i in range(0, display_range, 1):
+    length = max(
+            len(title_data), 
+            len(artist_data), 
+            len(country_data), 
+            len(company_data),
+            len(price_data), 
+            len(year_data))
+
+    if len(title_data) < length:
+        print("Invalid data: missing titles")
+        return
+
+    if len(artist_data) < length:
+        print("Invalid data: missing artists")
+        return
+
+    if len(country_data) < length:
+        print("Invalid data: missing countries")
+        return
+
+    if len(company_data) < length:
+        print("Invalid data: missing companies")
+        return
+
+    if len(price_data) < length:
+        print("Invalid data: missing prices")
+        return
+
+    if len(year_data) < length:
+        print("Invalid data: missing years")
+        return
+
+    for i in range(len(title_data)):
         print(f"{title_data[i]} - {artist_data[i]} - {country_data[i]} - {company_data[i]} - $ {price_data[i]} - {year_data[i]}")
 
 
